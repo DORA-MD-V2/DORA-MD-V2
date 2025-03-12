33f51a598b69ebfaa71ad2f3110b614818ed5edf
DORA-MD-V2 <mddora90@gmail.com>
2025-03-13 02:52:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "TYQUHJxI#KgBYUQ1JPHHNJddAL4BpyAKgb4O6YL1MW18yieDYVTQ",
   MONGODB: process.env.MONGODB || "mongodb://mongo:BWwDRowxYWJTnJwGMHmWDAIAxMkAkHgH@turntable.proxy.rlwy.net:34838",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
 };
