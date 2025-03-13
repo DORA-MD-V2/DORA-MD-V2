67143c18af215babf510380b59ac52f1884f46ec
DORA-MD 💀 <mddora90@gmail.com>
2025-03-13 17:29:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,6 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "TYQUHJxI#KgBYUQ1JPHHNJddAL4BpyAKgb4O6YL1MW18yieDYVTQ",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
-  PREFIX: process.emv.PREFIX || ".",
+  PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg"
 };
