7701d0ae23f757fd3af015bb701ced6ff357b1b2
DORA-MD 💀 <mddora90@gmail.com>
2025-03-13 17:31:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "TYQUHJxI#KgBYUQ1JPHHNJddAL4BpyAKgb4O6YL1MW18yieDYVTQ",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
   PREFIX: process.env.PREFIX || ".",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg"
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg",
+  ALIVE_MSG: process.rnv.ALIVE_MSG || "Hello , I am alive now!!\n\n💀𝐌𝐚𝐝𝐞 𝐛𝐲 R_A_S_𝐈_D_U💀"
 };
