da1e2422ddf4d72ac67bec222d6557688eb21080
DORA-MD 💀 <mddora90@gmail.com>
2025-03-13 17:37:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,7 @@ module.exports = {
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg",
-  ALIVE_MSG: process.rnv.ALIVE_MSG || "Hello , I am alive now!!\n\n💀𝐌𝐚𝐝𝐞 𝐛𝐲 R_A_S_𝐈_D_U💀"
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n💀𝐌𝐚𝐝𝐞 𝐛𝐲 R_A_S_𝐈_D_U💀",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE: process.env.MODE || "public",
 };
